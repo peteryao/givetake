a82679c3a1cf393afb911702615cd42a7b22c8b0
Peter Yao <peteryao916@gmail.com>
2013-05-04 21:44:43 -0700

    Charity changes.

--- volunteer/views.py
+++ volunteer/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, render_to_response, redirect
+
 from django.contrib.auth import authenticate, login, logout
 
 from django.conf import settings
