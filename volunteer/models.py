73c02f50de584f46040bdd9d3f72cbc9c4b36baa
Peter Yao <peteryao916@gmail.com>
2013-05-04 18:01:18 -0700

    Updated the index to take the Static Files. Added in models

--- volunteer/models.py
+++ volunteer/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+class Charity(models.Model):
+    name = models.CharField(max_length=256)
+    url = models.URLField(max_length=256, default=None)
+    description = models.TextField(max_length=1028)
+
+class Service(models.Model):
+    name = models.CharField(max_length=256)
+    available = models.BooleanField(default=False)
+    provider = models.ForeignKey(User)
+    charity = models.ForeignKey(Charity)
+    ammount = models.DecimalField(max_digits=5, decimal_places=2, default=10.00)
+    description = models.TextField(max_length=1028)
+
+    def __unicode__(self):
+        return self.name
+
+class Donation(models.Model):
+    service = models.ForeignKey(Service)
+    provider = models.ForeignKey(User, null=True, related_name='provider')
+    receiver = models.ForeignKey(User, null=True, related_name='receiver')
+
